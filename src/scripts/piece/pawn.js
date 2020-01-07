297be81edbe8e6a083128e07945457278dd7ed12
soali <soali3107@gmail.com>
2020-01-07 12:41:59 -0500

    Pawns can move forward taking 1 or 2 Steps and not move past another piece

--- src/scripts/piece/pawn.js
+++ src/scripts/piece/pawn.js
@@ -6,9 +6,12 @@ class Pawn extends Piece {
         this.status = false;
 
         this.moveDirections = this.moveDirections.bind(this);
+        this.direction = this.direction.bind(this);
         this.atStart  = this.atStart.bind(this);
-        this.fowardDirections = this.fowardDirections.bind(this);
-        this.fowardSteps = this.fowardSteps.bind(this);
+        this.allMoves = this.allMoves.bind(this);
+
+        this.forwardDirections = this.forwardDirections.bind(this);
+        this.forwardSteps = this.forwardSteps.bind(this);
         this.sideSteps = this.sideSteps.bind(this);
     }
     symbol() {
@@ -20,23 +23,56 @@ class Pawn extends Piece {
 
     moveDirections() {
         // console.log("movedPawn");
-        // console.log(this.fowardDirections())
-        return [this.fowardDirections()]
+        // console.log(this.forwardDirections())
+        // let mainMove = this.forwardDirections();
+
+        // Consists of forward moves
+            //Two Step Forward Moves
+        let allMoves = [];
+        if (this.forwardDirections().length > 0) {
+            // debugger;
+            allMoves.push(this.forwardDirections());
+            if (allMoves.length > 0 && this.atStart()) {
+                allMoves.push([this.direction() + this.forwardDirections()[0], this.forwardDirections()[1]])
+            }
+        }
+
+        return allMoves;
+        // if (this.atStart()){
+        //     return [mainMove, [this.direction() + mainMove[0], mainMove[1]]]
+        // }
+        // else{
+        //     return [mainMove]
+        // }
     }
-    //Assumption that this.position[1] should return the y-coordinate of the 2d array.
+
+    direction() {
+        return this.color === 'white' ? -1 : 1;
+    }
+
     atStart(){
-        return ( this.color === 'white' ? 1 : 6 ) ===  this.position[1]
+        return ( this.color === 'white' ? 6 : 1 ) ===  this.position[0]
     }
 
-
-    fowardDirections(){
-        let direction = this.color === 'white' ? -1 : 1;
-        return [direction + this.position[0], this.position[1]]
+    allMoves(){
+        //Consists of diagonal Killing  Moves
+        //Passant
     }
 
-    fowardSteps(){
-        let oneStep = [this.position[0], this.position[1] + this.fowardDirections()];
-        let twoStep = [this.position[0] + 2 * this.fowardDirections(), this.position[1]];
+    // Consists of 0-1 Steps Forward
+    forwardDirections(){
+        let forward = [this.direction() + this.position[0], this.position[1]];
+        if (this.board.rows[forward[0]][forward[1]].color === 'null'){
+            return forward;
+        }
+        else {
+            return [];
+        }
+    }
+
+    forwardSteps(){
+        let oneStep = [this.position[0], this.position[1] + this.forwardDirections()];
+        let twoStep = [this.position[0] + 2 * this.forwardDirections(), this.position[1]];
         let steps = [];
         if(this.board.isValidPosition(oneStep) && this.board.isEmpty(oneStep)){
             steps.push(oneStep)
@@ -52,7 +88,7 @@ class Pawn extends Piece {
         let oppositeColor = (this.color === 'white' ?  'Black'  : 'white');
         let steps = [];
         console.log(this.position);
-        let direction = this.fowardDirections();
+        let direction = this.forwardDirections();
         let  moves =  [ [this.position[0] + direction,  this.position[1] + 1], [this.position[0] + direction, this.position[1] - 1]];
         for(let i = 0; i < moves.length; i++){
             let coord = moves[i];
